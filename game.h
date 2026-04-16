dc78d28d08b8954a6fb06378f02d0f88bae203df
yuenn614 <luxun0911@gmail.com>
2026-04-17 01:38:14 +0800

    Refactor Game24 class and rename solve to hasSolution

--- game.h
+++ game.h
@@ -1,13 +1,4 @@
-#ifndef GAME_H
-#define GAME_H
-
-#include <vector>
-#include <string>
-using namespace std;
-
 class Game24 {
 public:
-    bool solve(vector<double> nums, vector<string> expr);
+    bool hasSolution(vector<double> nums, vector<string> expr);
 };
-
-#endif
