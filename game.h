d6d4878ffe9176ff634926c47a5012b8328e95a8
yuenn614 <luxun0911@gmail.com>
2026-04-17 01:32:39 +0800

    Create game.h

---
+++ game.h
@@ -0,0 +1,13 @@
+#ifndef GAME_H
+#define GAME_H
+
+#include <vector>
+#include <string>
+using namespace std;
+
+class Game24 {
+public:
+    bool solve(vector<double> nums, vector<string> expr);
+};
+
+#endif
